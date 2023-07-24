513d9f9ab6114b1707ef2b9ca4951f0585bb1086
stephanees020719 <stephanees020719@gmail.com>
2023-07-24 17:57:20 -0400

    starter code

---
+++ README.md
@@ -0,0 +1,5 @@
+This is My first class project.
+
+This is a trivia game about superheroes movies.
+
+Show what you know about Marvel universe and DC. Score points for every corrected answer you get. 
---
+++ script.js
@@ -0,0 +1,62 @@
+//superhero trivia game 
+//get score every correct answer 
+
+
+//1. Define array of superheroe trivia questions
+const triviaQuestions = [
+{
+    question:" What is Batman real name? ",
+    options:["Bruce Wayne", "Clark Kent","Peter Parker","Tony stark" ],
+    answer: "Bruce Wayne"
+},
+
+{
+    question: "What is the real name of Iron Man?",
+    options: [ "Tony Wayne", "Tony Stark", "Clark Kent", "Howard Stark"],
+    answer: "Tony Stark"
+  },
+  {
+    question:"What is Captain America super Power?",
+    options:["super Strength", "Super Fast", "Fly", "Super Genious"],
+    answer: "Super Strength"
+  },
+  {
+    question:"Where is the Time Stone hidden in Doctor Strange?",
+    options: ["The Star", "Eye of Agamoto", " Planet Earth ", "Avengers Building"],
+    answer: "Eye of Agamotto"
+  },
+  {
+    question: "What species is Loki?",
+    options:["Frost Giant", "Magician", "Human","Asgardian"],
+    answer:  "Frost Giant"
+  },
+  {
+    question: "What type of Doctor is Stephen Strange?",
+    options:[" Neurosurgeon","Cardiologist", "Trauma Surgeon","Orthopedic surgeon"],
+    answer: " Neurosurgeon"
+  },
+  {
+    question:"Who killed Tony Stark’s parents?",
+    options:["King Tachala", "The Winter Soldier","Rocket","Thanos"],
+    answer: "The Winter Soldier"
+  },
+  {
+    question: "What is the name of Iron Man’s loyal assistant?",
+    options:["Nathasha", "Happy", "Pepper Potts","Aunt May"],
+    answer: "Pepper Potts"
+  },
+  {
+    question: "Which villain got his distinct appearance from toxic chemicals at a plant?",
+    options:["Pengin", "poison Yvy", "Two Faced","Joker"],
+    answer: "Joker"
+  },
+  {
+    question: "Who is part of the justice leage?",
+    options:["Captain Marver", "Thanos", "Flash", "Hayley Quinn"],
+    answer: "Flash"
+  },
+
+];
+
+
+// 2. Create a function for the trivia game 
